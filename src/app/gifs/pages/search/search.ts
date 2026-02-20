275bda35d36e5f84352c7e3469817c700bc77e58
Aldo <sigfrido.calderoni@gmail.com>
2026-02-20 12:08:32 -0800

    Complete Search component, which displays gifs on the screen after searching

--- src/app/gifs/pages/search/search.ts
+++ src/app/gifs/pages/search/search.ts
@@ -1,8 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
+import { GifService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search.html'
 })
-export default class Search { }
+export default class Search {
+  gifService = inject(GifService);
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query: string) {
+    this.gifService.searchGifs(query).subscribe((resp) => {
+      this.gifs.set(resp);
+    });
+  }
+}
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { environment } from '@environments/environment';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -28,5 +29,20 @@ export class GifService {
       this.trendingGifsLoading.set(false);
       console.log({gifs});
     })
+  }
+
+  searchGifs(query: string) {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query
+      }
+    }).pipe(
+      map(({data}) => data),
+      map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+    );
+
+    // TODO Historial
   }
 }
